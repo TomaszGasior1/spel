9b84bd39246b73478d279e0a7c6113358ede236e
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-06-26 18:39:05 +0200

    Creating model and dao

---
+++ src/main/java/com/tomaszgasior/spring/spel/Offer.java
@@ -0,0 +1,9 @@
+package com.tomaszgasior.spring.spel;
+
+public class Offer {
+	private int id;
+	private String name;
+	private String email;
+	private String text;
+
+}
---
+++ src/main/java/com/tomaszgasior/spring/spel/OffersDAO.java
@@ -0,0 +1,11 @@
+package com.tomaszgasior.spring.spel;
+
+import java.util.List;
+
+public class OffersDAO {
+	
+	public List<Offer> getOffers(){
+		return null;
+	}
+
+}
