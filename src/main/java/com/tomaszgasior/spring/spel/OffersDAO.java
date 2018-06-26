904f055657c86ebd5ef9971e966fd9149f2f32f7
Tomasz Gąsior <tomasz.gasior1990@gmail.com>
2018-06-26 21:07:06 +0200

    creating DAO class

--- src/main/java/com/tomaszgasior/spring/spel/OffersDAO.java
+++ src/main/java/com/tomaszgasior/spring/spel/OffersDAO.java
@@ -2,8 +2,20 @@ package com.tomaszgasior.spring.spel;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class OffersDAO {
 	
+	private JdbcTemplate jdbc;
+	
+	
+	
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = jdbc;
+	}
+
+
+
 	public List<Offer> getOffers(){
 		return null;
 	}
